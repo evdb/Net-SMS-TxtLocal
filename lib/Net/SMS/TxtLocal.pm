4782fa1aa0b19c9f3188926f33b7b6ec89336336
Edmund von der Burg <evdb@ecclestoad.co.uk>
2010-06-12 14:25:11 +0100

    Start of project

---
+++ lib/Net/SMS/TxtLocal.pm
@@ -0,0 +1,118 @@
+use warnings;
+use strict;
+
+package Net::SMS::TxtLocal;
+
+use Carp;
+use 5.006;
+
+our $VERSION = '0.01';
+
+
+=head1 NAME
+
+Net::SMS::TxtLocal - Send SMS messages using txtlocal.co.uk
+
+=head1 SYNOPSIS
+
+    use Net::SMS::TxtLocal;
+
+=for author to fill in:
+    Brief code example(s) here showing commonest usage(s).
+    This section will be as far as many users bother reading
+    so make it as educational and exeplary as possible.
+  
+  
+=head1 DESCRIPTION
+
+=for author to fill in:
+    Write a full description of the module and its features here.
+    Use subsections (=head2, =head3) as appropriate.
+
+
+=head1 INTERFACE 
+
+=for author to fill in:
+    Write a separate section listing the public components of the modules
+    interface. These normally consist of either subroutines that may be
+    exported, or methods that may be called on objects belonging to the
+    classes provided by the module.
+
+
+=head1 DIAGNOSTICS
+
+=for author to fill in:
+    List every single error and warning message that the module can
+    generate (even the ones that will "never happen"), with a full
+    explanation of each problem, one or more likely causes, and any
+    suggested remedies.
+
+=over
+
+=item C<< Error message here, perhaps with %s placeholders >>
+
+[Description of error here]
+
+=item C<< Another error message here >>
+
+[Description of error here]
+
+[Et cetera, et cetera]
+
+=back
+
+
+=head1 BUGS
+
+No bugs have been reported.
+
+Please report any bugs or feature requests to
+C<bug-net-sms-txtlocal@rt.cpan.org>, or through the web interface at
+L<http://rt.cpan.org>.
+
+=head1 CONTRIBUTING
+
+The repository for this module is hosted here:
+L<http://github.com/evdb/Net-SMS-TxtLocal>
+
+Please feel free to fork, make changes and send me a patch :)
+
+=head1 AUTHOR
+
+Edmund von der Burg  C<< <evdb@cpan.org> >>
+
+
+=head1 LICENCE AND COPYRIGHT
+
+Copyright (c) 2010, Edmund von der Burg C<< <evdb@cpan.org> >>. All rights reserved.
+
+This module is free software; you can redistribute it and/or
+modify it under the same terms as Perl itself. See L<perlartistic>.
+
+
+=head1 DISCLAIMER OF WARRANTY
+
+BECAUSE THIS SOFTWARE IS LICENSED FREE OF CHARGE, THERE IS NO WARRANTY
+FOR THE SOFTWARE, TO THE EXTENT PERMITTED BY APPLICABLE LAW. EXCEPT WHEN
+OTHERWISE STATED IN WRITING THE COPYRIGHT HOLDERS AND/OR OTHER PARTIES
+PROVIDE THE SOFTWARE "AS IS" WITHOUT WARRANTY OF ANY KIND, EITHER
+EXPRESSED OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE
+ENTIRE RISK AS TO THE QUALITY AND PERFORMANCE OF THE SOFTWARE IS WITH
+YOU. SHOULD THE SOFTWARE PROVE DEFECTIVE, YOU ASSUME THE COST OF ALL
+NECESSARY SERVICING, REPAIR, OR CORRECTION.
+
+IN NO EVENT UNLESS REQUIRED BY APPLICABLE LAW OR AGREED TO IN WRITING
+WILL ANY COPYRIGHT HOLDER, OR ANY OTHER PARTY WHO MAY MODIFY AND/OR
+REDISTRIBUTE THE SOFTWARE AS PERMITTED BY THE ABOVE LICENCE, BE
+LIABLE TO YOU FOR DAMAGES, INCLUDING ANY GENERAL, SPECIAL, INCIDENTAL,
+OR CONSEQUENTIAL DAMAGES ARISING OUT OF THE USE OR INABILITY TO USE
+THE SOFTWARE (INCLUDING BUT NOT LIMITED TO LOSS OF DATA OR DATA BEING
+RENDERED INACCURATE OR LOSSES SUSTAINED BY YOU OR THIRD PARTIES OR A
+FAILURE OF THE SOFTWARE TO OPERATE WITH ANY OTHER SOFTWARE), EVEN IF
+SUCH HOLDER OR OTHER PARTY HAS BEEN ADVISED OF THE POSSIBILITY OF
+SUCH DAMAGES.
+
+=cut
+
+1;
---
+++ t/perlcritic.t
@@ -0,0 +1,11 @@
+#!perl
+
+use strict;
+use warnings;
+
+if ( !require Test::Perl::Critic ) {
+    Test::More::plan(
+        skip_all => "Test::Perl::Critic required for testing PBP compliance" );
+}
+
+Test::Perl::Critic::all_critic_ok();
